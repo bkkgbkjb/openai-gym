0cc3a3837f71a38e49f259cc7cab821db86d1b29
shidiyuan <shidiyuan@meituan.com>
2022-03-03 17:11:35 +0800

    feat: 性能优化

--- utils/agent.py
+++ utils/agent.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from argparse import Action
 from optparse import Option
 import numpy as np
 import gym
@@ -42,7 +43,7 @@ class Agent(Generic[O, S, A]):
         self,
         comps: Union[List[Literal["preprocess", "algorithm"]], Literal["all"]] = [],
     ):
-        self.ready_act: Optional[A] = None
+        self.ready_act: Optional[Tuple[A, int]] = None
         self.end = False
         self.episode: Episode[O, A] = []
 
@@ -54,18 +55,53 @@ class Agent(Generic[O, S, A]):
         if comps == "all" or "algorithm" in comps:
             self.algm.reset()
 
+        self.times = 0
+
     def toggleImprove(self, newImprov: bool):
         self.improv = newImprov
+
+    def format_act(self, act: Union[A, Tuple[A, int]]) -> Tuple[A, int]:
+        if isinstance(act, tuple):
+            assert act[1] > 0
+            return (cast(A, act[0]), act[1])
+        else:
+            return (act, 1)
+
+    def get_act(self) -> Union[None, Tuple[A, int]]:
+        if self.ready_act is None:
+            return None
+
+        assert self.ready_act[1] >= 0
+        if self.ready_act[1] > 0:
+            return self.ready_act
+
+        return None
+
+    def need_after_step(self) -> bool:
+        return (
+            self.improv
+            and self.algm.after_step_freq > 0
+            and self.times % self.algm.after_step_freq == 0
+        )
+
+    def need_on_termination(self) -> bool:
+        return (
+            self.improv
+            and self.algm.need_on_termination
+        )
 
     def step(self) -> Tuple[O, bool, Optional[Episode[O, A]]]:
         assert not self.end, "cannot step on a ended agent"
 
-        act = self.ready_act or self.algm.take_action(
-            self.preprocess.get_current_state(self.episode)
+        act = self.get_act() or self.format_act(
+            self.algm.take_action(self.preprocess.get_current_state(self.episode))
         )
 
-        (obs, rwd, stop, _) = self.env.step(act)
-        self.episode[-1] = (self.episode[-1][0], act, rwd)
+        (obs, rwd, stop, _) = self.env.step(act[0])
+
+        self.ready_act = (act[0], act[1] - 1)
+
+        self.episode[-1] = (self.episode[-1][0], act[0], rwd)
 
         obs = cast(O, obs)
         self.episode.append((obs, None, None))
@@ -73,21 +109,33 @@ class Agent(Generic[O, S, A]):
         self.ready_act = (
             None
             if stop
-            else self.algm.take_action(self.preprocess.get_current_state(self.episode))
+            else (
+                self.get_act()
+                or self.format_act(
+                    self.algm.take_action(
+                        self.preprocess.get_current_state(self.episode)
+                    )
+                )
+            )
         )
 
-        self.improv and self.algm.after_step(
-            (self.preprocess.get_current_state(self.episode), self.ready_act),
+        self.need_after_step() and self.algm.after_step(
+            (
+                self.preprocess.get_current_state(self.episode),
+                self.ready_act and self.ready_act[0],
+            ),
             self.preprocess.transform_history(self.episode[:-1]),
         )
 
         if not stop:
+            self.times += 1
             return (obs, stop, None)
 
         self.end = True
-        self.improv and self.algm.on_termination(
+        self.need_on_termination() and self.algm.on_termination(
             self.preprocess.transform_history(self.episode)
         )
+        self.times += 1
         return (obs, stop, self.episode)
 
     def render(self, mode: str):
--- utils/algorithm.py
+++ utils/algorithm.py
@@ -1,32 +1,37 @@
 from abc import abstractmethod
 from optparse import Option
-from typing import Optional, Protocol, Tuple, TypeVar, List
+from typing import Optional, Protocol, Tuple, TypeVar, List, Union
 import numpy as np
 from utils.common import Episode
 
 
-S = TypeVar('S')
-A = TypeVar('A')
+S = TypeVar("S")
+A = TypeVar("A")
 # Episode = List[Tuple[S, Optional[A], Optional[R]]]
 
 
 class AlgorithmInterface(Protocol[S, A]):
+    
 
     @abstractmethod
     def allowed_actions(self, state: S) -> List[A]:
         raise NotImplementedError()
 
     @abstractmethod
-    def take_action(self, state: S) -> A:
+    def take_action(self, state: S) -> Union[A, Tuple[A, int]]:
         raise NotImplementedError()
 
     @abstractmethod
     def after_step(self, sa: Tuple[S, Optional[A]], episode: Episode[S, A]):
         raise NotImplementedError()
+    
+    after_step_freq: int
 
     @abstractmethod
     def on_termination(self, episode: Episode[S, A]):
         raise NotImplementedError()
+    
+    need_on_termination: bool
 
     @abstractmethod
     def reset(self):
