e7ddb71cf0d86a3c6d749a9e2649a8445a0f3cfc
diyuanshi <shidiyuan@westlake.edu.cn>
2022-05-18 15:34:59 +0800

    feat: 优化了sac的算法代码

--- papers/sac/algorithm.py
+++ papers/sac/algorithm.py
@@ -7,23 +7,17 @@ from utils.common import (
     NotNoneStepGeneric,
 )
 from torch import nn
-import math
 from collections import deque
 import torch
 from utils.preprocess import PreprocessInterface
 from utils.algorithm import AlgorithmInterface
-import plotly.graph_objects as go
 from torch.distributions import Categorical, Normal
-from tqdm.autonotebook import tqdm
+from utils.nets import NeuralNetworks, layer_init
 
-from torchvision import transforms as T
-from utils.agent import Agent
-from gym.spaces import Box
-from typing import List, Tuple, Literal, Any, Optional, cast, Callable, Union, Iterable, Dict
-import gym
-import numpy.typing as npt
-from utils.env import PreprocessObservation, FrameStack, resolve_lazy_frames
+from typing import List, Tuple, Any, Optional, Callable, Dict
 import numpy as np
+
+from utils.replay_buffer import ReplayBuffer
 
 Observation = torch.Tensor
 Action = torch.Tensor
@@ -38,7 +32,7 @@ NotNoneStep = NotNoneStepGeneric[State, ActionInfo[Action]]
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-class VFunction(nn.Module):
+class VFunction(NeuralNetworks):
     def __init__(self, n_state: int):
         super().__init__()
         self.net = nn.Sequential(
@@ -46,14 +40,14 @@ class VFunction(nn.Module):
             nn.ReLU(),
             layer_init(nn.Linear(256, 256)),
             nn.ReLU(),
-            layer_init(nn.Linear(256, 1))
+            layer_init(nn.Linear(256, 1)),
         ).to(DEVICE)
 
     def forward(self, s: State) -> torch.Tensor:
         return self.net(s.to(DEVICE))
 
 
-class QFunction(nn.Module):
+class QFunction(NeuralNetworks):
     def __init__(self, n_state: int, n_action: int):
         super().__init__()
         self.net = nn.Sequential(
@@ -61,7 +55,7 @@ class QFunction(nn.Module):
             nn.ReLU(),
             layer_init(nn.Linear(256, 256)),
             nn.ReLU(),
-            layer_init(nn.Linear(256, 1))
+            layer_init(nn.Linear(256, 1)),
         ).to(DEVICE)
 
     def forward(self, s: State, a: Action) -> torch.Tensor:
@@ -70,7 +64,7 @@ class QFunction(nn.Module):
         return self.net(torch.cat([s.to(DEVICE), a.to(DEVICE)], 1))
 
 
-class PaiFunction(nn.Module):
+class PaiFunction(NeuralNetworks):
     def __init__(self, n_state: int, n_action):
         super().__init__()
         self.net = nn.Sequential(
@@ -114,15 +108,9 @@ class PaiFunction(nn.Module):
         return act, log_prob, mean
 
 
-def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-    torch.nn.init.orthogonal_(layer.weight, std)
-    torch.nn.init.constant_(layer.bias, bias_const)
-    return layer
-
-
 class SAC(AlgorithmInterface[State, Action]):
     def __init__(self, n_state: int, n_actions: int):
-        self.name = 'sac'
+        self.name = "sac"
         self.n_actions = n_actions
         self.n_state = n_state
 
@@ -131,17 +119,11 @@ class SAC(AlgorithmInterface[State, Action]):
         self.alpha = 1.0
         self.tau = 1e-2
 
-        self.replay_memory_size = int(1e6)
         self.start_traininig_size = int(1e4)
         self.mini_batch_size = 256
 
         self.online_v = VFunction(self.n_state)
-        self.offline_v = VFunction(self.n_state)
-        self.offline_v.load_state_dict(self.online_v.state_dict())
-        for p in self.offline_v.parameters():
-            p.requires_grad = False
-
-        self.offline_v.eval()
+        self.offline_v = self.online_v.clone().no_grad()
 
         self.policy = PaiFunction(self.n_state, self.n_actions)
 
@@ -164,12 +146,11 @@ class SAC(AlgorithmInterface[State, Action]):
         pass
 
     def set_reporter(self, reporter: Callable[[Dict[str, Any]], None]):
-        self.reporter = reporter
+        self.report = reporter
 
     def reset(self):
         self.times = 0
-        self.replay_memory: deque[Transition] = deque(
-            maxlen=self.replay_memory_size)
+        self.replay_memory = ReplayBuffer[State, Action]()
 
     def on_agent_reset(self):
         pass
@@ -179,56 +160,30 @@ class SAC(AlgorithmInterface[State, Action]):
         action, _, _ = self.policy.sample(state.unsqueeze(0))
         return action.detach().cpu().squeeze(0).numpy()
 
-    def on_episode_termination(self, sar: Tuple[List[State], List[ActionInfo[Action]], List[Reward]]):
+    def on_episode_termination(
+        self, sar: Tuple[List[State], List[ActionInfo[Action]], List[Reward]]
+    ):
         pass
 
-    def after_step(self, sar: Tuple[State, ActionInfo[Action], Reward], sa: Tuple[State, Optional[ActionInfo[Action]]]):
+    def after_step(
+        self,
+        sar: Tuple[State, ActionInfo[Action], Reward],
+        sa: Tuple[State, Optional[ActionInfo[Action]]],
+    ):
         (s, a, r) = sar
         (sn, an) = sa
         self.replay_memory.append((s, a, r, sn, an))
 
-        if len(self.replay_memory) >= self.start_traininig_size:
+        if self.replay_memory.len >= self.start_traininig_size:
             self.train()
 
         self.times += 1
 
-    def get_mini_batch(self) -> List[Transition]:
-        idx = np.random.choice(len(self.replay_memory), self.mini_batch_size)
-        l = list(self.replay_memory)
-
-        r = []
-        for i in idx:
-            r.append(l[i])
-
-        return r
-
-    def get_batch_details(self, mini_batch: List[Transition]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
-
-        states = torch.stack([s for (s, _, _, _, _) in mini_batch])
-        assert states.shape == (self.mini_batch_size, 17)
-
-        actions = torch.stack([torch.from_numpy(a).type(torch.float32)
-                               for (_, (a, _), _, _, _) in mini_batch])
-        assert actions.shape == (self.mini_batch_size, 6)
-
-        rewards = torch.stack([torch.tensor(r, dtype=torch.float32)
-                               for (_, _, r, _, _) in mini_batch]).unsqueeze(1)
-        assert rewards.shape == (self.mini_batch_size, 1)
-
-        next_states = torch.stack([sn for (_, _, _, sn, _) in mini_batch])
-        assert next_states.shape == (self.mini_batch_size, 17)
-
-        done = torch.as_tensor(
-            [1 if an is None else 0 for (_, _, _, _, an) in mini_batch], dtype=torch.int8).unsqueeze(1)
-        assert done.shape == (self.mini_batch_size, 1)
-
-        return (states.to(DEVICE), actions.to(DEVICE), rewards.to(DEVICE), next_states.to(DEVICE), done.to(DEVICE))
-
     def train(self):
-        mini_batch = self.get_mini_batch()
 
-        (states, actions, rewards, next_states,
-         done) = self.get_batch_details(mini_batch)
+        (states, actions, rewards, next_states, done) = ReplayBuffer.resolve(
+            self.replay_memory.sample(self.mini_batch_size)
+        )
 
         new_actions, new_log_probs, _ = self.policy.sample(states)
 
@@ -236,8 +191,7 @@ class SAC(AlgorithmInterface[State, Action]):
         predicted_q1 = self.q1(states, actions)
         predicted_q2 = self.q2(states, actions)
         target_val = self.offline_v(next_states)
-        target_q_val = (rewards + (1 - done) *
-                        self.gamma * target_val).detach()
+        target_q_val = (rewards + (1 - done) * self.gamma * target_val).detach()
 
         q_val_loss1 = self.q1_loss(predicted_q1, target_q_val)
         q_val_loss2 = self.q2_loss(predicted_q2, target_q_val)
@@ -253,10 +207,12 @@ class SAC(AlgorithmInterface[State, Action]):
         # Training V Function
         predicted_val = self.online_v(states)
         predicted_new_q_value = torch.min(
-            self.q1(states, new_actions), self.q2(states, new_actions))
+            self.q1(states, new_actions), self.q2(states, new_actions)
+        )
 
-        target_value_func = (predicted_new_q_value -
-                             self.alpha * new_log_probs).detach()
+        target_value_func = (
+            predicted_new_q_value - self.alpha * new_log_probs
+        ).detach()
 
         value_loss = self.v_loss(predicted_val, target_value_func)
 
@@ -265,24 +221,22 @@ class SAC(AlgorithmInterface[State, Action]):
         self.v_optimizer.step()
 
         # Training P Function
-        policy_loss = (self.alpha * new_log_probs -
-                       predicted_new_q_value).mean()
+        policy_loss = (self.alpha * new_log_probs - predicted_new_q_value).mean()
         self.p_optimizer.zero_grad()
         policy_loss.backward()
         self.p_optimizer.step()
 
         # soft update old_v net
-        for old_params, params in zip(self.offline_v.parameters(), self.online_v.parameters()):
-            old_params.data.copy_(
-                old_params.data * (1.0 - self.tau) + params.data * self.tau
-            )
+        self.offline_v.soft_update_to(self.online_v, self.tau)
 
-        self.reporter(dict(
-            value_loss=value_loss,
-            policy_loss=policy_loss,
-            q_loss1=q_val_loss1,
-            q_loss2=q_val_loss2
-        ))
+        self.report(
+            dict(
+                value_loss=value_loss,
+                policy_loss=policy_loss,
+                q_loss1=q_val_loss1,
+                q_loss2=q_val_loss2,
+            )
+        )
 
 
 class Preprocess(PreprocessInterface[Observation, Action, State]):
--- papers/sac/main.py
+++ papers/sac/main.py
@@ -1,131 +1,25 @@
 import setup
-from time import sleep, time
-from algorithm import SAC, Observation, Preprocess
-import torch
-from os import times
-import plotly.graph_objects as go
-from tqdm import tqdm
+from algorithm import SAC, Preprocess
 from utils.agent import Agent
-from typing import Dict, List, Any
 import gym
-from gym.wrappers import RecordVideo
-from algorithm import State, Observation, Action
-from utils.env import PreprocessObservation, FrameStack, ToTensorEnv
-from utils.env_sb3 import WarpFrame, MaxAndSkipEnv, NoopResetEnv, EpisodicLifeEnv
-import numpy as np
-from torch.utils.tensorboard import SummaryWriter
-
-# %%
-RANDOM_SEED = 0
-np.random.seed(RANDOM_SEED)
-torch.manual_seed(RANDOM_SEED)
-torch.cuda.manual_seed_all(RANDOM_SEED)
-
-# %%
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
-# %%
-writer = SummaryWriter()
-
-# %%
-
-
-def eval(a: Agent[Observation, State, Action]):
-    a.toggleEval(True)
-
-    for _ in range(3):
-        input("press any key to start eval agent")
-        a.reset()
-
-        while True:
-            a.render(mode="human")
-
-            (_, s) = a.step()
-
-            if s:
-                print(f"rwd is: {np.sum(a.reward_episode)}, steps: {a.steps}")
-                break
-
-
-def glance():
-    env = gym.make("Walker2d-v2")
-    env.seed(RANDOM_SEED)
-    env.action_space.seed(RANDOM_SEED)
-    env.observation_space.seed(RANDOM_SEED)
-    env.reset()
-
-    print(env.action_space, env.observation_space)
-    for _ in range(3):
-        r = 0.0
-        env.reset()
-        s = False
-        t = 0
-
-        while not s:
-            env.render(mode="human")
-
-            (_, rwd, stop, _) = env.step(env.action_space.sample())
-            t += 1
-
-            r += rwd
-
-            if stop:
-                print(f"rwd is: {r}, steps: {t}")
-                break
+from setup import RANDOM_SEED
+from utils.reporter import get_reporter
+from utils.env import train_and_eval
 
 
 # %%
 
+env = gym.make("Walker2d-v2")
+env.seed(RANDOM_SEED)
+env.action_space.seed(RANDOM_SEED)
+env.observation_space.seed(RANDOM_SEED)
+env.reset()
 
-def train() -> Agent[Observation, State, Action]:
-    env = gym.make("Walker2d-v2")
-    env.seed(RANDOM_SEED)
-    env.action_space.seed(RANDOM_SEED)
-    env.observation_space.seed(RANDOM_SEED)
-    env = RecordVideo(env, "rl-video")
-    env.reset()
-
-    TRAINING_TIMES = int(4e5)
-
-    print(env.action_space, env.observation_space)
-    agent = Agent(env, SAC(17, 6), Preprocess())
-    agent.reset()
-    print(f"train agent: {agent.name}")
-
-    j = 0
-
-    def report(info: Dict[str, Any]):
-        nonlocal j
-        for k, v in info.items():
-            writer.add_scalar(k, v, j)
-        j += 1
-
-    agent.set_algm_reporter(report)
-
-    with tqdm(total=TRAINING_TIMES) as pbar:
-        frames = 0
-        epi = 0
-        while frames < TRAINING_TIMES:
-            agent.reset()
-            i = 0
-            end = False
-            while not end and frames < TRAINING_TIMES:
-
-                (_, end) = agent.step()
-                i += 1
-
-            epi += 1
-            frames += i
-
-            pbar.update(i)
-
-            rwd = np.sum([r for r in agent.reward_episode])
-            writer.add_scalar("reward", rwd, epi)
-        print(f"training end after {frames} frames")
-
-    return agent
+agent = Agent(
+    env, SAC(env.observation_space.shape[0], env.action_space.shape[0]), Preprocess()
+)
 
 
-# %%
-eval(train())
+agent.set_algm_reporter(get_reporter())
+
+train_and_eval(agent)
