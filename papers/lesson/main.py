d8af0a7fdeaa48d77d019ca06ad841ceb39db5e4
diyuanshi <bkkgbkjb@gmail.com>
2022-06-26 23:36:45 +0800

    feat: 移除了visualize_goal

--- papers/lesson/main.py
+++ papers/lesson/main.py
@@ -25,6 +25,9 @@ eval_env = RecordVideo(
 )
 train_env, eval_env = make_train_and_eval_env((train_env, eval_env), [], RANDOM_SEED)
 
+train_env.env.visualize_goal = False
+eval_env.env.visualize_goal = False
+
 # %%
 
 agent = Agent(
